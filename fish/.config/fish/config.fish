f1306a27f4078090a45eac9521d1f9735f641aa4
Ivan Lomakin <shplender77@gmail.com>
2022-01-23 21:55:54 +0300

    Added aliases for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,8 @@ set -x MANPAGER "nvim -c 'set ft=man' -"            # nvim as manpager
 starship init fish | source                         # Starship prompt
 fzf_configure_bindings --directory=\ct              # Ctrl+t to find file
 bind -M insert \cf accept-autosuggestion
+
+alias cp="cp -iv"                    # confirm before overwriting something
+alias rm='rm -iv'
+alias mv='mv -iv'
+alias tldr="tldr --linux --theme ocean"
