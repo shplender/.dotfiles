7e132a1d436f9a9f40b201a7fd06f4e8669b31a7
Ivan Lomakin <shplender77@gmail.com>
2022-04-10 09:24:05 +0300

    testing ssh added ssh-agency package

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -13,7 +13,7 @@
 ;; (package! python-info)
 (package! tree-sitter)
 (package! tree-sitter-langs)
-
+(package! ssh-agency)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
